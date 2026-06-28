2895e2e8d0059baa06725b0b6305c8499dc48b01
Emilly12321 <emillysilvaluz2839@gmail.com>
2026-06-28 18:24:54 -0300

    fix

--- VUE_3.x.x/OT10/Backend/server.js
+++ VUE_3.x.x/OT10/Backend/server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+
+const banco = require('./Config/database');
+
+const authRouter = require('./src/Routes/authRouter');
+const produtoRouter = require('./src/Routes/productRouter');
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.listen()
+app.use('/auth', authRouter);
+app.use('/produtos', produtoRouter);
+
+banco.sync().then(() => {
+
+    app.listen(3000, () => {
+        console.log('Servidor rodando em http://localhost:3000');
+    });
+
+}).catch((error) => {
+    console.log(error);
+});
--- VUE_3.x.x/OT10/Frontend/src/main.js
+++ VUE_3.x.x/OT10/Frontend/src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './Router';
+import store from './Store';
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+    components,
+    directives
+});
+
+createApp(App)
+    .use(router)
+    .use(store)
+    .use(vuetify)
+    .mount('#app');
